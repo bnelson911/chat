d9b28512ddcf7f0209ed2e686537bad217d4d125
fpn <fpn@bebo.com>
2016-10-09 05:21:06 +0000

    iframely workarounds for not giving out the final size and hiding onload

--- src/js/utils/Iframely.js
+++ src/js/utils/Iframely.js
@@ -1,6 +1,31 @@
 import React from 'react';
 import 'whatwg-fetch';
 import Url from 'url';
+// import _ from 'lodash';
+
+var onMessage = function(e) {
+  // console.log(e.origin, JSON.stringify(e.data));
+  if (typeof(e.data) ==='string') {
+    try {
+      var data = JSON.parse(e.data);
+      if (data.method === "resize" || data.method === "widgetRendered") {
+        var iframes = document.getElementsByTagName("iframe");
+        for (var i=0; i< iframes.length; i++) {
+          var f = iframes[i];
+          if (e.source === f.contentWindow) {
+            console.log("found iframe", f, data);
+            var event = new Event("resize");
+            event.height = data.height;
+            _.defer(() => f.dispatchEvent(event));
+          }
+        }
+      }
+    } catch (err) {
+    }
+  }
+}
+
+var subscribed = false;
 
 class Iframely extends React.Component {
 
@@ -8,6 +33,11 @@ class Iframely extends React.Component {
     super();
     this.state = {
       iframelyResponse: null,
+      height: "200px",
+    }
+    if (!subscribed) {
+      window.addEventListener("message", onMessage, false);
+      subscribed = true;
     }
   }
 
@@ -24,6 +54,7 @@ class Iframely extends React.Component {
     u.query = {"api_key": "a09c4dfedf62021c92999a",
                "url": url,
                "omit_script": true,
+               "lazy": false,
                "iframe": true};
     fetch(Url.format(u))
     .then(res => res.json())
@@ -42,7 +73,49 @@ class Iframely extends React.Component {
   }
 
   componentDidMount() {
+    // window.iframely && iframely.load();
+  }
+
+  componentDidUpdate() {
     window.iframely && iframely.load();
+    var embed = this.refs.embed;
+    if (!embed) {
+      return;
+    }
+
+    var iframes = embed.getElementsByTagName("iframe");
+    if (iframes && iframes.length >0) {
+      var iframe = iframes[0];
+      iframe.onload = (event) => {
+          console.log("onLoad", this.props.url, event.currentTarget.offsetHeight);
+          setTimeout(() => this.onResize(event, embed), 100);
+      };
+      iframe.onresize= (event) => {
+          console.log("resize", this.props.url, event.currentTarget.offsetHeight);
+          this.onResize(event, embed);
+      };
+    }
+
+    this.onResize({}, embed);
+  }
+
+  onResize(event, embed) {
+    console.log("onResize", event, embed);
+    var height;
+    if (event.height) {
+      height = event.height + "px";;
+      if (event.currentTarget && event.currentTarget.offsetHeight) {
+        height = event.currentTarget.offsetHeight + "px";
+      }
+    } else {
+      if (embed.children && embed.children.length >0) {
+        height = embed.children[0].offsetHeight + "px";
+      }
+    }
+    console.log("iframely.onResize", event.type, this.props.url, height);
+    if (height !== this.state.height) {
+      this.setState({height});
+    }
   }
 
   getIframelyHtml() {
@@ -64,12 +137,11 @@ class Iframely extends React.Component {
 
     var style = {
       width: Bebo.Utils.isMobile() ? "100%" : "50%",
-      height: "120px",
-      maxHeight: "120px",
+      height: this.state.height,
       overflowY: "hidden",
     };
 
-    return <div style={style} dangerouslySetInnerHTML={this.getIframelyHtml()} />
+    return <div ref="embed" style={style} dangerouslySetInnerHTML={this.getIframelyHtml()} />
   }
   
 }
